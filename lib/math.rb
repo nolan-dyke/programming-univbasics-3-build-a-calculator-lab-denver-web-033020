ea2bfaefe493a2b6aa41025cbebe66b7c2e1d7d1
Nolan Dyke <nolan.g.dyke@gmail.com>
2020-02-19 20:28:14 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,18 +5,22 @@ end
 
 def subtraction(num1, num2)
   sum = num1 - num2 
+  sum
 end
 
 def division(num1, num2)
   sum = num1 / num2
+  sum 
 end
 
 def multiplication(num1, num2)
   sum = num1 * num2
+  sum 
 end
 
 def modulo(num1, num2)
   sum = num1 % num2
+  sum 
 end
 
 def square_root(num)
